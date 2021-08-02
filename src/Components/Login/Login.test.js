ab98295d71086cef2956c9bd8a0d6eeb363dab1d
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-02 18:24:47 +0200

    delete duplicated tests

--- src/Components/Login/Login.test.js
+++ src/Components/Login/Login.test.js
@@ -3,6 +3,13 @@ import Login from './index';
 import Input from './Input';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+    ...jest.requireActual('react-redux'),
+    useSelector: jest.fn(),
+    useDispatch: () => mockDispatch,
+}));
+
 describe("<Login page='signup' /> component", () => {
     const wrapper = mount(
         <Router>
@@ -26,6 +33,20 @@ describe("<Login page='signup' /> component", () => {
         expect(value).toEqual(expectedValue);
         expect(lastInput.props().value).toEqual('');
     });
+
+    it('should call mockDispatch on submit', () => {
+        const inputs = wrapper.find('input');
+        inputs.forEach((input) => {
+            if (input.props().name === 'email') {
+                input.simulate('change', { target: { value: 'test@test.fr' } });
+            } else {
+                input.simulate('change', { target: { value: 'TestTest' } });
+            }
+        });
+        const form = wrapper.find('form');
+        form.simulate('submit');
+        expect(mockDispatch).toHaveBeenCalled();
+    });
 });
 
 describe("<Login page='login' component />", () => {
@@ -41,15 +62,5 @@ describe("<Login page='login' component />", () => {
 
     it('should contain 2 inputs', () => {
         expect(wrapper.find(Input).length).toEqual(2);
-    });
-
-    it('should write in one input', () => {
-        const expectedValue = 'Test';
-        const input = wrapper.find('input').first();
-        const lastInput = wrapper.find('input').last();
-        input.simulate('change', { target: { value: expectedValue } });
-        const value = wrapper.find('input').first().props().value;
-        expect(value).toEqual(expectedValue);
-        expect(lastInput.props().value).toEqual('');
     });
 });
