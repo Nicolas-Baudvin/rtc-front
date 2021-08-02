40474d733d7a1391961ebad38f511610cadde7f8
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-02 16:45:25 +0200

    add Home page

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,12 +1,16 @@
 import './style.scss';
 import Login from '../Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../Home';
 
 function App() {
     return (
         <div className="App">
             <Router>
                 <Switch>
+                    <Route exact path={'/'}>
+                        <Home />
+                    </Route>
                     <Route exact path={'/connexion'}>
                         <Login page={'login'} />
                     </Route>
