46a900fc6a682ec3ea840dc56589c5cb5fc0a1d9
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-02 12:13:58 +0200

    tests & add router

--- src/Components/App/App.test.jsx
+++ src/Components/App/App.test.jsx
@@ -1,14 +1,15 @@
-import { shallow } from "enzyme";
-import App from "./index";
+import { shallow } from 'enzyme';
+import App from './index';
 import Login from '../Login';
 
 describe('<App />', () => {
-        const wrapper = shallow(<App />);
+    const wrapper = shallow(<App />);
     it('should render', () => {
         expect(wrapper).toBeTruthy();
     });
 
-    it("should contain Login component", () => {
-        expect(wrapper.contains(<Login />)).toEqual(true);
+    it('should contain Login component', () => {
+        expect(wrapper.contains(<Login page={'signup'} />)).toEqual(true);
+        expect(wrapper.contains(<Login page={'login'} />)).toEqual(true);
     });
-})
+});
--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,12 +1,22 @@
 import './style.scss';
 import Login from '../Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path={'/connexion'}>
+                        <Login page={'login'} />
+                    </Route>
+                    <Route exact path={'/inscription'}>
+                        <Login page={'signup'} />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
