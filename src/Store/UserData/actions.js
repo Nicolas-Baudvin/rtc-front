7738480c7c0b0931a68a7274913b20d58bd6c4e0
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-03 18:09:22 +0200

    set up an error handler & dispatch errors on Message reducer

--- src/Store/UserData/actions.js
+++ src/Store/UserData/actions.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { newErrorMessage } from '../Message/actions';
 
 export const NEW_USER_DATA = 'NEW_USER_DATA';
-export const NEW_USER_DATA_ERROR = 'NEW_USER_DATA_ERROR';
 export const FETCHING_NEW_USER_DATA = 'FETCHING_NEW_USER_DATA';
+export const LOGOUT = 'LOGOUT';
 
 export const fetchingNewUserData = () => ({
     type: FETCHING_NEW_USER_DATA,
@@ -13,14 +14,16 @@ export const newUserData = (payload) => ({
     payload,
 });
 
-export const newUserDataError = (payload) => ({
-    type: NEW_USER_DATA_ERROR,
-    payload,
+export const logout = () => ({
+    type: LOGOUT,
 });
 
 function getUserData(api, payload) {
     return axios({
-        url: 'http://localhost:5000/api/user/',
+        url:
+            payload.page === 'signup'
+                ? 'http://localhost:5000/api/user/'
+                : 'http://localhost:5000/api/user/auth/',
         method: 'post',
         data: {
             ...payload,
@@ -28,18 +31,28 @@ function getUserData(api, payload) {
     });
 }
 
+function handleError(error, dispatch) {
+    if (error?.response?.status === 403) {
+        dispatch(logout());
+        return 'Accès refusé';
+    }
+    if (error?.response?.data?.error) {
+        return error.response.data.error;
+    } else if (error?.response?.data?.errors) {
+        return error.response.data.errors[0].msg;
+    } else {
+        return 'Une erreur est survenue avec le serveur.';
+    }
+}
+
 export function fetchUserData(payload) {
     return function (dispatch, getState, { api }) {
         dispatch(fetchingNewUserData());
 
         return getUserData(api, payload)
-            .then((user) => dispatch(newUserData(user)))
-            .catch((err) =>
-                dispatch(
-                    newUserDataError({
-                        message: 'Une erreur est survenue',
-                    })
-                )
+            .then((user) => dispatch(newUserData(user.data)))
+            .catch((error) =>
+                dispatch(newErrorMessage(handleError(error, dispatch)))
             );
     };
 }
