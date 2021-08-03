55572e37b49e34342d00cd7fda957959680fcd60
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-03 18:10:21 +0200

    add message reducer

--- src/Store/rootReducer.js
+++ src/Store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import userReducer from './UserData/reducer';
+import messageReducer from './Message/reducer';
 
 export const rootReducer = combineReducers({
     user: userReducer,
+    popup: messageReducer,
 });
 
 export default rootReducer;
