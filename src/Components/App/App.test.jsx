3ab1dae59c475d720c8ab69d7c00aa56804b5c0e
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-03 12:08:49 +0200

    mock useSelector

--- src/Components/App/App.test.jsx
+++ src/Components/App/App.test.jsx
@@ -1,9 +1,21 @@
 import { shallow } from 'enzyme';
 import App from './index';
 import Login from '../Login';
+import { useSelector as useSelectorMock } from 'react-redux';
+
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+    ...jest.requireActual('react-redux'),
+    useSelector: jest.fn(),
+    useDispatch: () => mockDispatch,
+}));
 
 describe('<App />', () => {
+    useSelectorMock.mockImplementation(() => ({
+        token: '',
+    }));
     const wrapper = shallow(<App />);
+
     it('should render', () => {
         expect(wrapper).toBeTruthy();
     });
