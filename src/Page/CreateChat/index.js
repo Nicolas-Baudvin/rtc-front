068bf0d23295cd3bbf675e50e08f617cc1356411
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-16 11:24:52 +0200

    replace current form by reusable Form Component

--- src/Page/CreateChat/index.js
+++ src/Page/CreateChat/index.js
@@ -1,15 +1,75 @@
 import Header from '../../Components/Header';
 import './style.scss';
-import Form from './Form';
+import Form from '../../Components/Form';
+import { useReducer } from 'react';
+import { initialState, reducer } from './reducer';
+import { useHistory } from 'react-router-dom';
+
+const makeAction = (type, payload) => ({
+    type,
+    payload,
+});
+
+const dispatchByInputName = (inputName, dispatch) =>
+    ({
+        roomName: (e) => dispatch(makeAction('NEW_ROOM_NAME', e.target.value)),
+        roomPass: (e) => dispatch(makeAction('NEW_ROOM_PASS', e.target.value)),
+    }[inputName]);
+
+const inputs = [
+    {
+        inputProps: {
+            name: 'roomName',
+            type: 'text',
+            info: 'Le nom du salon ne doit pas contenir de caractères spéciaux et dois faire entre 3 et 40 caractères',
+        },
+        labelProps: {
+            htmlFor: 'roomName',
+        },
+        labelTitle: 'Nom du salon',
+    },
+    {
+        inputProps: {
+            name: 'roomPass',
+            type: 'password',
+            info: 'Le mot de passe du salon doit faire moins de 30 caractères (mot de passe complexe conseillé avec caractères spéciaux)',
+        },
+        labelProps: {
+            htmlFor: 'roomPass',
+        },
+        labelTitle: 'Mot de passe du salon',
+    },
+];
 
 function CreateChat() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const history = useHistory();
+
     const onSubmit = (e) => {
         e.preventDefault();
     };
+
+    const onClickBack = () => history.push('/dashboard');
+
+    const onChange = (e, inputName) => {
+        dispatchByInputName(inputName, dispatch)(e);
+    };
+
     return (
         <div className={'create'}>
             <Header page={'Créer un salon'} />
-            <Form onSubmit={onSubmit} />
+            <Form
+                onSubmit={onSubmit}
+                onChange={onChange}
+                inputs={inputs}
+                state={state}
+                isLoading={false}
+                errors={{}}
+            />
+            <p> ou </p>
+            <button onClick={onClickBack} className={'button button-border'}>
+                Retour
+            </button>
         </div>
     );
 }
