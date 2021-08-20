227005fad7e60ea1674c671b9030aae1fc45a3c9
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-20 12:11:41 +0200

    add rooms store to combince reducer

--- src/Store/rootReducer.js
+++ src/Store/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import userReducer from './UserData/reducer';
 import messageReducer from './Message/reducer';
 import serverReducer from './WebSocket/reducer';
+import roomsReducer from './Rooms/reducer';
 
 export const rootReducer = combineReducers({
     user: userReducer,
     popup: messageReducer,
     server: serverReducer,
+    rooms: roomsReducer,
 });
 
 export default rootReducer;
