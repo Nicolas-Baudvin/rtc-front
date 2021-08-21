5681f9bb98f2940fbd90ddcf617f0508fcb1f076
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-21 11:40:01 +0200

    style & add a title

--- src/Page/MyRooms/index.js
+++ src/Page/MyRooms/index.js
@@ -1,9 +1,10 @@
 import Header from '../../Components/Header';
 import { useDispatch, useSelector } from 'react-redux';
 import { FaCrown } from 'react-icons/fa';
-import { AiOutlineStar } from 'react-icons/ai';
+import { MdClear } from 'react-icons/md';
 import { useEffect } from 'react';
 import { getRooms } from '../../Store/Rooms/actions';
+import './style.scss';
 
 function MyRooms() {
     const dispatch = useDispatch();
@@ -17,12 +18,13 @@ function MyRooms() {
         <div className={'roomsList'}>
             <Header page={'Mes Salons'} />
             <div className={'roomsList-list'}>
+                <p>Cliquez sur le salon que vous voulez rejoindre</p>
                 {all.map((room) => (
                     <div className={'roomsList-list__item'}>
                         <div>{room.name}</div>
                         <div>
                             {username === room.owner.username && <FaCrown />}
-                            <AiOutlineStar />
+                            <MdClear />
                         </div>
                     </div>
                 ))}
