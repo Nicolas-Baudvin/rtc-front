f69f501a0a4ca3633a57670c87cf2d5ddf1a1182
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-25 11:58:49 +0200

    display all chats where user is member of and redirect on it when clicking on it

--- src/Page/MyRooms/index.js
+++ src/Page/MyRooms/index.js
@@ -3,13 +3,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import { FaCrown } from 'react-icons/fa';
 import { MdClear } from 'react-icons/md';
 import { useEffect } from 'react';
-import { getRooms } from '../../Store/Rooms/actions';
+import { getRooms, newCurrentRoom } from '../../Store/Rooms/actions';
 import './style.scss';
+import { useHistory } from 'react-router-dom';
 
 function MyRooms() {
     const dispatch = useDispatch();
     const { all, isLoading } = useSelector((state) => state.rooms);
     const { username } = useSelector((state) => state.user);
+    const history = useHistory();
+
+    const onClick = (room) => {
+        dispatch(newCurrentRoom(room));
+        history.push(`/salons/${room.name}/${room._id}`);
+    };
+
     useEffect(() => {
         dispatch(getRooms());
     }, []);
@@ -19,8 +27,12 @@ function MyRooms() {
             <Header page={'Mes Salons'} />
             <div className={'roomsList-list'}>
                 <p>Cliquez sur le salon que vous voulez rejoindre</p>
-                {all.map((room) => (
-                    <div className={'roomsList-list__item'}>
+                {all.map((room, i) => (
+                    <div
+                        onClick={() => onClick(room)}
+                        key={i}
+                        className={'roomsList-list__item'}
+                    >
                         <div>{room.name}</div>
                         <div>
                             {username === room.owner.username && <FaCrown />}
