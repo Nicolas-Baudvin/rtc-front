3658a4ec8a22730719d31aae7d3f4c31058c6c44
Nicolas-Baudvin <nicolas.baudvin@gmail.com>
2021-08-31 13:01:08 +0200

    disconnect from current room when coming on dashboard page

--- src/Page/Dashboard/index.js
+++ src/Page/Dashboard/index.js
@@ -5,18 +5,26 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { connect } from '../../Store/WebSocket/actions';
+import { disconnectFromRoom } from '../../Store/Rooms/actions';
 
 function Dashboard() {
     const history = useHistory();
     const dispatch = useDispatch();
     const { token } = useSelector((state) => state.user);
     const { socket } = useSelector((state) => state.server);
+    const { current } = useSelector((state) => state.rooms);
 
     useEffect(() => {
         if (!token) {
             history.push('/connexion');
         }
     }, [token]);
+
+    useEffect(() => {
+        if (current && socket) {
+            dispatch(disconnectFromRoom());
+        }
+    }, [current, socket]);
 
     useEffect(() => {
         if (!socket) {
